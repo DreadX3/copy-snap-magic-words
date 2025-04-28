83fc70bb242065ba66d8e01c0820960450054620
DreadX3 <dreadx3@gmail.com>
2025-04-28 20:12:10 -0300

    Troca imagem Index demo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -36,7 +36,7 @@ const Index = () => {
                   <div className="relative block w-full bg-white rounded-lg overflow-hidden">
                     <img
                       className="w-full"
-                      src="https://images.unsplash.com/photo-1556742502-ec7c0e9f34b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80"
+                      src="https://plus.unsplash.com/premium_photo-1682125177822-63c27a3830ea?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2787&q=80"
                       alt="CopySnap AI Demo"
                     />
                     <div className="absolute inset-0 w-full h-full flex items-center justify-center">
